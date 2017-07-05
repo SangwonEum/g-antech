c76016f9740df4eb78d5480a62de626419b4a076
SangwonEum <eachdaycount87@gmail.com>
2017-07-05 17:57:09 +0900

    final submit

--- Android project progress/Intent/app/src/main/java/com/example/ganedu/intent/NextActivity.java
+++ Android project progress/Intent/app/src/main/java/com/example/ganedu/intent/NextActivity.java
@@ -1,16 +1,30 @@
 package com.example.ganedu.intent;
+
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by ganedu on 2017-07-05.
  */
 
 public class NextActivity extends Activity {
-
+    Button btnBack;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.next);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+
+
+            }
+        });
     }
+
+
 }
